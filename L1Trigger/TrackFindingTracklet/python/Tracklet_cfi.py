a0988eb905d67846c8c74ae19b0f0e7f1dcddac6
Emyr Clement <emyr.clement@bristol.ac.uk>
2018-02-11 20:51:13 +0000

    Include TMTT settings in Tracklet config.

--- L1Trigger/TrackFindingTracklet/python/Tracklet_cfi.py
+++ L1Trigger/TrackFindingTracklet/python/Tracklet_cfi.py
@@ -1,4 +1,5 @@
 import FWCore.ParameterSet.Config as cms
+from TMTrackTrigger.TMTrackFinder.TMTrackProducer_Defaults_cfi import TMTrackProducer_params
 
 TTTracksFromTracklet = cms.EDProducer("L1TrackProducer",
                                       SimTrackSource = cms.InputTag("g4SimHits"),
@@ -10,5 +11,14 @@ TTTracksFromTracklet = cms.EDProducer("L1TrackProducer",
                                       TrackingVertexInputTag = cms.InputTag("mix", "MergedTrackTruth"),
                                       BeamSpotSource = cms.InputTag("offlineBeamSpot"),
                                       asciiFileName = cms.untracked.string(""),
-                                      trackerGeometryType  = cms.untracked.string("")  #tilted barrel is assumed, use "flat" if running on flat
-                                      )
+                                      trackerGeometryType  = cms.untracked.string(""),  #tilted barrel is assumed, use "flat" if running on flat
+                                      tmttSettings = TMTrackProducer_params # Include TMTT settings
+    )
+
+# Remove digitisation in TMTT modules
+TMTrackProducer_params.StubDigitize.EnableDigitize  = cms.bool(False)
+
+# Inflate residual cuts in TMTT fitters (whilst getting the chain to work)
+TMTrackProducer_params.TrackFitSettings.ResidualCut = cms.double(20.0)
+TMTrackProducer_params.TrackFitSettings.GeneralResidualCut = cms.double(20.0)
+TMTrackProducer_params.TrackFitSettings.KillingResidualCut = cms.double(20.0)
