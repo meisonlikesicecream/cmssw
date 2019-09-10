110785f62088d5b5f754f5a8d345c9e27854e23e
Emyr <emyr.clement@bristol.ac.uk>
2019-09-10 21:05:44 +0100

    Fix default baselines

--- SimGeneral/MixingModule/python/SiStripSimParameters_cfi.py
+++ SimGeneral/MixingModule/python/SiStripSimParameters_cfi.py
@@ -128,16 +128,16 @@ SiStripSimBlock = cms.PSet(
     apvBaselines_zBinEdges     =  cms.vdouble(),
     apvBaselines_puBinEdges    =  cms.vdouble(),
     apvBaselines_nBinsPerBaseline  =  cms.uint32( 82 ),
-    apvBaselinesFile_tib1      = cms.FileInPath('SimTracker/SiStripDigitizer/data/APVBaselines_TIB1_12us.txt'),
-    apvBaselinesFile_tib2      = cms.FileInPath('SimTracker/SiStripDigitizer/data/APVBaselines_TIB2_15us.txt'),
-    apvBaselinesFile_tib3      = cms.FileInPath('SimTracker/SiStripDigitizer/data/APVBaselines_TIB3_16us.txt'),
+    apvBaselinesFile_tib1      = cms.FileInPath('SimTracker/SiStripDigitizer/data/APVBaselines_TIB1_11us.txt'),
+    apvBaselinesFile_tib2      = cms.FileInPath('SimTracker/SiStripDigitizer/data/APVBaselines_TIB2_14us.txt'),
+    apvBaselinesFile_tib3      = cms.FileInPath('SimTracker/SiStripDigitizer/data/APVBaselines_TIB3_15us.txt'),
     apvBaselinesFile_tib4      = cms.FileInPath('SimTracker/SiStripDigitizer/data/APVBaselines_TIB4_17us.txt'),
-    apvBaselinesFile_tob1      = cms.FileInPath('SimTracker/SiStripDigitizer/data/APVBaselines_TOB1_10us.txt'),
-    apvBaselinesFile_tob2      = cms.FileInPath('SimTracker/SiStripDigitizer/data/APVBaselines_TOB2_13us.txt'),
-    apvBaselinesFile_tob3      = cms.FileInPath('SimTracker/SiStripDigitizer/data/APVBaselines_TOB3_16us.txt'),
+    apvBaselinesFile_tob1      = cms.FileInPath('SimTracker/SiStripDigitizer/data/APVBaselines_TOB1_9us.txt'),
+    apvBaselinesFile_tob2      = cms.FileInPath('SimTracker/SiStripDigitizer/data/APVBaselines_TOB2_11us.txt'),
+    apvBaselinesFile_tob3      = cms.FileInPath('SimTracker/SiStripDigitizer/data/APVBaselines_TOB3_14us.txt'),
     apvBaselinesFile_tob4      = cms.FileInPath('SimTracker/SiStripDigitizer/data/APVBaselines_TOB4_17us.txt'),
-    apvBaselinesFile_tob5      = cms.FileInPath('SimTracker/SiStripDigitizer/data/APVBaselines_TOB5_17us.txt'),
-    apvBaselinesFile_tob6      = cms.FileInPath('SimTracker/SiStripDigitizer/data/APVBaselines_TOB6_18us.txt')
+    apvBaselinesFile_tob5      = cms.FileInPath('SimTracker/SiStripDigitizer/data/APVBaselines_TOB5_21us.txt'),
+    apvBaselinesFile_tob6      = cms.FileInPath('SimTracker/SiStripDigitizer/data/APVBaselines_TOB6_21us.txt')
 )
 
 from Configuration.ProcessModifiers.premix_stage1_cff import premix_stage1
